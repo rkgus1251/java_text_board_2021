010139808c5388cc8268b2ed82434f2e540fd032
rkgus1251 <gahyun1251@gmail.com>
2021-11-16 12:37:53 +0900

    NO2

--- src/com/sgh/exam/board/Main.java
+++ src/com/sgh/exam/board/Main.java
@@ -3,7 +3,9 @@ package com.sgh.exam.board;
 public class Main {
 
   public static void main(String[] args) {
-    // write your code here
+    System.out.println(" == 게시판 v0.1 == ");
+    System.out.println(" == 프로그램 시작 == ");
+    System.out.println("명령 ) ");
+    System.out.println(" == 프로그램 종료 == ");
   }
 }
-
