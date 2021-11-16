feace7aadefe7856334dec719e3b80048a1ccf62
rkgus1251 <gahyun1251@gmail.com>
2021-11-16 12:51:07 +0900

    명령어 입력

--- src/com/sgh/exam/board/Main.java
+++ src/com/sgh/exam/board/Main.java
@@ -1,11 +1,19 @@
 package com.sgh.exam.board;
 
+
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
     System.out.println(" == 게시판 v0.1 == ");
     System.out.println(" == 프로그램 시작 == ");
-    System.out.println("명령 ) ");
+    System.out.printf("명령 ) ");
+    Scanner in = new Scanner(System.in);
+    String cmd = in.nextLine();
+    System.out.printf("입력된 명령어 : %s\n", cmd);
     System.out.println(" == 프로그램 종료 == ");
+    in.close();
+
   }
 }
